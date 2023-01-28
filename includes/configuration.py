4516c5ad1dd9a882f9a567ea52829b92b9653390
Ricardo <rikigermanramos@gmail.com>
2023-01-28 16:27:13 -0300

    notebooks created

--- includes/configuration.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-raw_folder_path = "/mnt/formula1dlriki/raw"
-processed_folder_path = "/mnt/formula1dlriki/processed"
-presentation_folder_path = "/mnt/formula1dlriki/presentation"
